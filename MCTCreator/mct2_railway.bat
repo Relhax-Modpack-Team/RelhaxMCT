adbf48e75c83d27fa995e6bd1918405edba4555f
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2021-05-02 11:16:43 +0200

    Update mct2_railway.bat

--- MCTCreator/mct2_railway.bat
+++ MCTCreator/mct2_railway.bat
@@ -3,6 +3,6 @@
 MCTCreator2.exe /wot-path "F:\World_of_Tanks\" /CTM-mod "DD7744"
 
 F:
-cd World_of_Tanks\mods\1.12.1.0
+cd World_of_Tanks\mods\1.12.1.1
 del railway.wotmod
 ren mod.MCTCreator.TrainMapTextures_*.wotmod railway.wotmod
