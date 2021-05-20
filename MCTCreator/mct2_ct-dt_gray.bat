9c61e459551d091898433e6d560b518dd5d85273
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2021-05-20 14:57:40 +0200

    1.12.1.2

--- MCTCreator/mct2_ct-dt_gray.bat
+++ MCTCreator/mct2_ct-dt_gray.bat
@@ -3,7 +3,7 @@
 MCTCreator2.exe /wot-path "F:\World_of_Tanks\" /DPT-mod "BCBBBB" /CCT-mod "BCBBBB" /inside-color "FFFFFF" /remove-shadows
 
 F:
-cd World_of_Tanks\mods\1.12.1.1
+cd World_of_Tanks\mods\1.12.1.2
 del crashedTracksBase.wotmod
 del deadTanksBase.wotmod
 ren mod.MCTCreator.TrackCrashTextures_*.wotmod crashedTracksBase.wotmod
