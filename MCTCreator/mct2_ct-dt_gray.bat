fbc9455ee731e8ed573232439c9cca083e11b2d4
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2021-08-11 12:37:44 +0200

    bat -> 1.14.0.0

--- MCTCreator/mct2_ct-dt_gray.bat
+++ MCTCreator/mct2_ct-dt_gray.bat
@@ -3,7 +3,7 @@
 MCTCreator2.exe /wot-path "F:\World_of_Tanks\" /DPT-mod "BCBBBB" /CCT-mod "BCBBBB" /inside-color "FFFFFF" /remove-shadows
 
 F:
-cd World_of_Tanks\mods\1.13.0.0
+cd World_of_Tanks\mods\1.14.0.0
 del crashedTracksBase.wotmod
 del deadTanksBase.wotmod
 ren mod.MCTCreator.TrackCrashTextures_*.wotmod crashedTracksBase.wotmod
--- MCTCreator/mct2_railway.bat
+++ MCTCreator/mct2_railway.bat
@@ -3,6 +3,6 @@
 MCTCreator2.exe /wot-path "F:\World_of_Tanks\" /CTM-mod "DD7744"
 
 F:
-cd World_of_Tanks\mods\1.13.0.0
+cd World_of_Tanks\mods\1.14.0.0
 del railway.wotmod
 ren mod.MCTCreator.TrainMapTextures_*.wotmod railway.wotmod
